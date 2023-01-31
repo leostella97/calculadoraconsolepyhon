7a19031da6110a72a8d5ced2116202918533b610
Leonardo Stella <leonstella97@gmail.com>
2023-01-31 14:57:29 -0300

    Upload do programa feito em arquivo .py

---
+++ calculadora.py
@@ -0,0 +1,43 @@
+opcao = input("1 PARA SOMA | 2 PARA SUBTRAÇÃO | 3 PARA MULTIPLICAÇÃO \n 4 PARA DIVISÃO | 5 PARA MODULAR | 6 PARA EXPONENCIAÇÃO \n Escreva a opção desejada: ")
+
+if opcao == "1":
+    print("### Soma ###")
+    x = input("Digite o primeiro número: ")
+    y = input("Digite o segundo número: ")
+    resultado = float(x)+float(y)
+    print("O resultado é: ", resultado)
+
+elif opcao == "2":
+    print("### Subtração ###")
+    x = input("Digite o primeiro número: ")
+    y = input("Digite o segundo número: ")
+    resultado = float(x)-float(y)
+    print("O resultado é: ", resultado)
+
+elif opcao == "3":
+    print("### Multiplicação ###")
+    x = input("Digite o primeiro número: ")
+    y = input("Digite o segundo número: ")
+    resultado = float(x)*float(y)
+    print("O resultado é: ", resultado)
+
+elif opcao == "4":
+    print("### Divisão ###")
+    x = input("Digite o primeiro número: ")
+    y = input("Digite o segundo número: ")
+    resultado = float(x)/float(y)
+    print("O resultado é: ", resultado)
+
+elif opcao == "5":
+    print("### Modular ###")
+    x = input("Digite o primeiro número: ")
+    y = input("Digite o segundo número: ")
+    resultado = float(x)%float(y)
+    print("O resultado é: ", resultado)
+
+elif opcao == "6":
+    print("### Exponenciação ###")
+    x = input("Digite o primeiro número: ")
+    y = input("Digite o segundo número: ")
+    resultado = float(x)**float(y)
+    print("O resultado é: ", resultado)
