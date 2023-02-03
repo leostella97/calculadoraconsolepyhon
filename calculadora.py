880a6a24b21dc12157e13f30dab11165f2de0f92
Leonardo Stella de Oliveira <Leonstella97@gmail.com>
2023-02-03 10:04:42 -0300

    Substituição do menu para while para enquanto a opção não for escolhida repetir as opções e substituição das opções de string para int simplificando o código

--- calculadora.py
+++ calculadora.py
@@ -1,46 +1,46 @@
-opcao = input("1 PARA SOMA | 2 PARA SUBTRAÇÃO | 3 PARA MULTIPLICAÇÃO \n 4 PARA DIVISÃO | 5 PARA MODULAR | 6 PARA EXPONENCIAÇÃO \n Escreva a opção desejada: ")
+#opcao = input("1 PARA SOMA | 2 PARA SUBTRAÇÃO | 3 PARA MULTIPLICAÇÃO \n 4 PARA DIVISÃO | 5 PARA MODULAR | 6 PARA EXPONENCIAÇÃO \n Escreva a opção desejada: ")
+opcao = -1
+while opcao != 0:
+    opcao = int(input("1 PARA SOMA | 2 PARA SUBTRAÇÃO | 3 PARA MULTIPLICAÇÃO \n 4 PARA DIVISÃO | 5 PARA MODULAR | 6 PARA EXPONENCIAÇÃO \n 0 PARA SAIR \n Escreva a opção desejada: "))
 
-if opcao == "1":
-    print("### Soma ###")
-    x = input("Digite o primeiro número: ")
-    y = input("Digite o segundo número: ")
-    resultado = float(x)+float(y)
-    print("O resultado é: ", resultado)
+    if opcao == 1:
+        print("### Soma ###")
+        x = input("Digite o primeiro número: ")
+        y = input("Digite o segundo número: ")
+        resultado = float(x)+float(y)
+        print("O resultado é: ", resultado, end="\n\n")
 
-elif opcao == "2":
-    print("### Subtração ###")
-    x = input("Digite o primeiro número: ")
-    y = input("Digite o segundo número: ")
-    resultado = float(x)-float(y)
-    print("O resultado é: ", resultado)
+    elif opcao == 2:
+        print("### Subtração ###")
+        x = input("Digite o primeiro número: ")
+        y = input("Digite o segundo número: ")
+        resultado = float(x)-float(y)
+        print("O resultado é: ", resultado, end="\n\n")
 
-elif opcao == "3":
-    print("### Multiplicação ###")
-    x = input("Digite o primeiro número: ")
-    y = input("Digite o segundo número: ")
-    resultado = float(x)*float(y)
-    print("O resultado é: ", resultado)
+    elif opcao == 3:
+        print("### Multiplicação ###")
+        x = input("Digite o primeiro número: ")
+        y = input("Digite o segundo número: ")
+        resultado = float(x)*float(y)
+        print("O resultado é: ", resultado, end="\n\n")
 
-elif opcao == "4":
-    print("### Divisão ###")
-    x = input("Digite o primeiro número: ")
-    y = input("Digite o segundo número: ")
-    resultado = float(x)/float(y)
-    print("O resultado é: ", resultado)
+    elif opcao == 4:
+        print("### Divisão ###")
+        x = input("Digite o primeiro número: ")
+        y = input("Digite o segundo número: ")
+        resultado = float(x)/float(y)
+        print("O resultado é: ", resultado, end="\n\n")
 
-elif opcao == "5":
-    print("### Modular ###")
-    x = input("Digite o primeiro número: ")
-    y = input("Digite o segundo número: ")
-    resultado = float(x)%float(y)
-    print("O resultado é: ", resultado)
+    elif opcao == 5:
+        print("### Modular ###")
+        x = input("Digite o primeiro número: ")
+        y = input("Digite o segundo número: ")
+        resultado = float(x)%float(y)
+        print("O resultado é: ", resultado, end="\n\n")
 
-elif opcao == "6":
-    print("### Exponenciação ###")
-    x = input("Digite o primeiro número: ")
-    y = input("Digite o segundo número: ")
-    resultado = float(x)**float(y)
-    print("O resultado é: ", resultado)
-
-else:
-    print("Por favor, escolha uma opção válida")
+    elif opcao == 6:
+        print("### Exponenciação ###")
+        x = input("Digite o primeiro número: ")
+        y = input("Digite o segundo número: ")
+        resultado = float(x)**float(y)
+        print("O resultado é: ", resultado, end="\n\n")
