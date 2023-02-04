137d7875ff241d63c934f290bc990ed6c86bb760
Leonardo Stella de Oliveira <Leonstella97@gmail.com>
2023-02-04 00:21:01 -0300

    Alteração do menu para string de múltiplas linhas para melhor visualização

--- calculadora.py
+++ calculadora.py
@@ -1,7 +1,12 @@
 #opcao = input("1 PARA SOMA | 2 PARA SUBTRAÇÃO | 3 PARA MULTIPLICAÇÃO \n 4 PARA DIVISÃO | 5 PARA MODULAR | 6 PARA EXPONENCIAÇÃO \n Escreva a opção desejada: ")
+print("===================== CALCULADORA =====================")
 opcao = -1
 while opcao != 0:
-    opcao = int(input("1 PARA SOMA | 2 PARA SUBTRAÇÃO | 3 PARA MULTIPLICAÇÃO \n 4 PARA DIVISÃO | 5 PARA MODULAR | 6 PARA EXPONENCIAÇÃO \n 0 PARA SAIR \n Escreva a opção desejada: "))
+    opcao = int(input("""1 PARA SOMA | 2 PARA SUBTRAÇÃO | 3 PARA MULTIPLICAÇÃO
+4 PARA DIVISÃO | 5 PARA MODULAR | 6 PARA EXPONENCIAÇÃO
+===================== 0 PARA SAIR =====================
+
+Escreva a opção desejada: """))
 
     if opcao == 1:
         print("### Soma ###")
